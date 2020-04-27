2c485727a0f5b52ba430887318ee85478cab48c4
Carlos A. Garcia <carlosalbertogarcia.b@gmail.com>
2020-04-27 10:55:33 -0500

    Testing...

--- src/index.js
+++ src/index.js
@@ -7,3 +7,10 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+console.log("Listening on port 8000");
+console.log(process.versions);
+console.log("Exiting with success!");
+process.exitCode = 0;
+console.log("Exit Code: " + process.exitCode);
+process.exit()
